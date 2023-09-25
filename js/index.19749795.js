edf18c013091f5b663a573f77740d8a74ca3dc4f
小逸 <2429016980.com>
2023-09-25 15:02:50 +0800

    123123

--- js/index.19749795.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./index.591fe32d.js";function r(t){return e({url:"/talk/list",method:"get",params:t})}function a(){return e({url:"/home/talk",method:"get"})}function o(t){return e({url:`/talk/${t}`,method:"get"})}function u(t){return e({url:`/talk/${t}/like`,method:"post"})}export{r as a,o as b,a as g,u as l};
